f8ff7139f225ed9f007c8f58cc5d9c117d6d4567
dev_chow <56135351+devchow@users.noreply.github.com>
2021-10-15 16:00:30 +0300

    commit x
    commit x

--- Assets/Scripts/Fish.cs
+++ Assets/Scripts/Fish.cs
@@ -30,6 +30,32 @@ public class Fish : MonoBehaviour
 
     public Fish()
     {
-        
+        Bonus = BonusType.None;
+    }
+    
+    // Comparing Fish
+    public bool IsSameType(Fish newFish)
+    {
+        return string.Compare(this.Type, newFish.Type) == 0;
+    }
+    
+    // custom constructor
+    public void Initialize(string type, int row, int column)
+    {
+        Column = column;
+        Row = row;
+        Type = type;
+    }
+    
+    // Swapping Rows & Columns
+    public static void SwapRowColumn(Fish f1, Fish f2)
+    {
+        int temp = f1.Row;
+        f1.Row = f2.Row;
+        f2.Row = temp;
+
+        temp = f1.Column;
+        f1.Column = f2.Column;
+        f2.Column = temp;
     }
 }
---
+++ Assets/Scripts/MatchChecker.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatchChecker
+{
+    public static IEnumerator AnimatePotentialMatches(IEnumerable<GameObject> potentialMatches)
+    {
+        for (float i = 1.0f; i >= 0.3f; i -= 0.1f)
+        {
+            foreach (var item in potentialMatches)
+            {
+                Color c = item.GetComponent<SpriteRenderer>().color;
+                c.a = i; // Setting Color Aplha
+            }
+
+            yield return new WaitForSeconds(GameVariables.OpacityAnimationDelay);
+        }
+        
+        for (float i = 0.3f; i <= 1.0f; i += 0.1f)
+        {
+            foreach (var item in potentialMatches)
+            {
+                Color c = item.GetComponent<SpriteRenderer>().color;
+                c.a = i; // Setting Color Aplha
+            }
+            
+            yield return new WaitForSeconds(GameVariables.OpacityAnimationDelay);
+        }
+    } // PotentialAnimation
+    
+    // Checking if Fish are Horizontal or Vertical Neighbours
+    public static bool AreHorizontalVerticalNeighbours(Fish f1, Fish f2)
+    {
+        return (f1.Column == f2.Column || f1.Row == f2.Row) && Mathf.Abs(f1.Column - f2.Column) <= 1 &&
+               Mathf.Abs(f1.Row - f2.Row) <= 1;
+    }
+    
+} // Match Checker
