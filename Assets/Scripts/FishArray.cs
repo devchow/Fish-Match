d50ae396c3234509814321d4cd2fd80912697fdd
dev_chow <56135351+devchow@users.noreply.github.com>
2021-10-15 17:17:08 +0300

    commit x
    commit x

--- Assets/Scripts/FishArray.cs
+++ Assets/Scripts/FishArray.cs
@@ -55,4 +55,109 @@ public class FishArray
 
     }
     
+    // Undo Swap Function
+    public void UndoSwap()
+    {
+        Swap(backup1, backup2);
+        
+    } //Undo-Swap
+    
+    // Get Horizontal Matches
+    private IEnumerable<GameObject> GetMatchesHorizontally(GameObject go)
+    {
+        List<GameObject> matches = new List<GameObject>();
+        matches.Add(go);
+
+        var fish = go.GetComponent<Fish>();
+        
+        // Search Left
+        if (fish.Column != 0)
+        {
+            for (int column = fish.Column - 1; column >= 0; column--)
+            {
+                if (fishes[fish.Row, column].GetComponent<Fish>().IsSameType(fish))
+                {
+                    matches.Add(fishes[fish.Row, column]);
+                }
+                else
+                {
+                    break;
+                }
+            }
+        } // S_L
+        
+        // Search Right
+        if (fish.Column != GameVariables.Columns - 1)
+        {
+            for (int column = fish.Column + 1; column < GameVariables.Columns; column++)
+            {
+                if (fishes[fish.Row, column].GetComponent<Fish>().IsSameType(fish))
+                {
+                    matches.Add(fishes[fish.Row, column]);
+                }
+                else
+                {
+                    break;
+                }
+            }
+        } // S-R
+
+        if (matches.Count < GameVariables.MinMatches)
+        {
+            matches.Clear();
+        }
+
+        return matches.Distinct(); // return non-duplicate matches
+        
+    } // Get-Matches-Horizontally
+    
+    // Get Vertical Matches
+    private IEnumerable<GameObject> GetMatchesVertically(GameObject go)
+    {
+        List<GameObject> matches = new List<GameObject>();
+        matches.Add(go);
+
+        var fish = go.GetComponent<Fish>();
+        
+        // Search Bottom
+        if (fish.Row != 0)
+        {
+            for (int row = fish.Row - 1; row >= 0; row--)
+            {
+                if (fishes[row, fish.Column].GetComponent<Fish>().IsSameType(fish))
+                {
+                    matches.Add(fishes[row, fish.Column]);
+                }
+                else
+                {
+                    break;
+                }
+            }
+        } // S_B
+        
+        // Search Top
+        if (fish.Row != GameVariables.Rows - 1)
+        {
+            for (int row = fish.Column + 1; row < GameVariables.Columns; row++)
+            {
+                if (fishes[row, fish.Column].GetComponent<Fish>().IsSameType(fish))
+                {
+                    matches.Add(fishes[row, fish.Column]);
+                }
+                else
+                {
+                    break;
+                }
+            }
+        } // S-T
+
+        if (matches.Count < GameVariables.MinMatches)
+        {
+            matches.Clear();
+        }
+
+        return matches.Distinct();  // return non-duplicate matches
+        
+    } // Get-Matches-Vertically
+    
 } // FishArray
